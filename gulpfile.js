bc07f2ccb261de1b29c7212043e90ac858ec93ce
annmirosh <mac@Ann-2.local>
2015-09-16 22:19:17 +0500

    Livereload task was added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
-// Include gulp
-var gulp = require('gulp');
-var isTravis = process.env.TRAVIS || false;
-
-// Include Plugins
-var wiredep = require('wiredep').stream;
-var Server = require('karma').Server;
+// Include gulp and plugins
+var gulp = require('gulp'),
+  livereload = require('gulp-livereload'),
+  wiredep = require('wiredep').stream,
+  Server = require('karma').Server,
+  isTravis = process.env.TRAVIS || false;
 
 // Add files from bower to index.html automatically
 gulp.task('bower', function () {
@@ -19,9 +18,15 @@ gulp.task('test', function (done) {
     configFile: __dirname + '/karma.conf.js',
     singleRun: isTravis,
     autoWatch: true,
-    browsers: isTravis?[ 'PhantomJS' ]:[ 'Chrome' ]
+    browsers: isTravis ? [ 'PhantomJS' ] : [ 'Chrome' ]
   }, done).start();
 });
 
+//Watch task
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch('./src/**/*.*').on('change', livereload.changed);
+});
+
 // Default Task
-gulp.task('default', [ 'bower' ]);
+gulp.task('default', [ 'bower', 'livereload' ]);
