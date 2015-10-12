3db2d2be0d51f9e5b541a0f115cbd2f96be9c1cd
annmirosh <mac@Ann.Dlink>
2015-10-12 20:15:22 +0500

    Build was fixed (#5)

--- protractor-conf.js
+++ protractor-conf.js
@@ -21,3 +21,13 @@ exports.config = {
     defaultTimeoutInterval: 30000
   }
 };
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
+}
