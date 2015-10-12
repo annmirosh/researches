bd7b2ba72b36f29fa825a8bc879eb84bb00f7678
annmirosh <mac@Ann.Dlink>
2015-10-12 20:26:56 +0500

    Build was fixed (#6)

--- tasks/protractor.js
+++ tasks/protractor.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
-  protractor = require("gulp-protractor").protractor;
+  protractor = require('gulp-protractor').protractor;
 
 gulp.task('protractor', function () {
   return gulp.src([ "src/**/*e2e.js" ])
     .pipe(protractor({
-      configFile: "protractor-conf.js"
+      configFile: 'protractor-conf.js'
     }))
     .on('error', function (e) {
       throw e
